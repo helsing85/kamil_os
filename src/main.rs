75bb206477c8e34bb215d0649df1165fe139c994
helsing85 <helsing85@gmail.com>
2022-07-29 17:57:17 +0200

    My OS. Minimal freestanding Rust binary

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+// kamil_os with Rust
+// https://os.phil-opp.com/freestanding-rust-binary/
+
+// An example for such a bare metal environment is the thumbv7em-none-eabihf target triple,
+// which describes an embedded ARM system. The details are not important,
+// all that matters is that the target triple has no underlying operating system,
+// which is indicated by the none in the target triple.
+// rustup target add thumbv7em-none-eabihf
+
+// Command to build for bare-metal
+// cargo build --target thumbv7em-none-eabihf
+
+// main.rs
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+/// Overwriting the operating system entry point with our own _start function:
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
