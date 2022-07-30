ceaeefea31e227da1b4db289d44737e35089472f
helsing85 <helsing85@gmail.com>
2022-07-30 20:14:20 +0200

    Test framwork with output on the device screen. Auto closing of QEMU is disabled.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 // main.rs
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
@@ -22,5 +25,43 @@ pub extern "C" fn _start() -> ! {
     println!("0b{:08b}/0b{:08b} = 0d{}", 1, 3, 1.0 / 3.0);
 
     //panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+//------------------------------------------
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    //exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
