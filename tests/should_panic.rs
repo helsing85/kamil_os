c7a594fe1e5b24556c4d39d652f76ea4c18fef82
helsing85 <helsing85@gmail.com>
2022-07-31 12:48:52 +0200

    'Should panic' test made simpler.

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,37 +2,21 @@
 
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use kamil_os::serial_print;
-use kamil_os::{exit_qemu, serial_println, QemuExitCode};
+use kamil_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 //------------------------------------------
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
-
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
 //------------------------------------------
-
-// Only the first function is executed
-// because the execution cannot continue
-// after the panic handler has been called.
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -43,7 +27,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 //------------------------------------------
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
