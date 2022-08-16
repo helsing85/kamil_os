c95da2a5d979c8cfdb632aeda89adbd747ce497c
helsing85 <helsing85@gmail.com>
2022-08-16 19:36:11 +0200

    Cleaning uneeded code.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use kamil_os::{
     memory::{self, BootInfoFrameAllocator},
     print, println,
 };
-use x86_64::{structures::paging::Page, VirtAddr};
+use x86_64::VirtAddr;
 
 //------------------------------------------
 
@@ -28,16 +28,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     kamil_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     #[cfg(test)]
     test_main();
