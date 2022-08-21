8e4986a0cedf2aab485636fef21381790923782b
helsing85 <helsing85@gmail.com>
2022-08-21 17:48:49 +0200

    Added simple executor for async functions.

---
+++ src/task/mod.rs
@@ -0,0 +1,23 @@
+// src/task/mod.rs
+
+pub mod simple_executor;
+
+use alloc::boxed::Box;
+use core::task::{Context, Poll};
+use core::{future::Future, pin::Pin};
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+}
+
+impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Self {
+        Task {
+            future: Box::pin(future),
+        }
+    }
+
+    fn poll(&mut self, context: &mut Context) -> Poll<()> {
+        self.future.as_mut().poll(context)
+    }
+}
